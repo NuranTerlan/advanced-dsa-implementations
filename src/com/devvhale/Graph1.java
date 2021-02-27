e1541954ce7ff7b3fa3ce116cc2a843bc3541597
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-02-28 02:41:24 +0400

    Update Graph1.java

--- src/com/devvhale/Graph1.java
+++ src/com/devvhale/Graph1.java
@@ -2,29 +2,29 @@ package com.devvhale;
 
 import java.util.LinkedList;
 
-public class Graph {
+public class Graph1 {
     private int vertices;
     private LinkedList<Integer> adjList[];
 
-    public Graph(int vertices) {
+    public Graph1(int vertices) {
         this.vertices = vertices;
         adjList = new LinkedList[vertices];
-        for(int i = 0; i < vertices; i++) {
+        for (int i = 0; i < vertices; i++) {
             adjList[i] = new LinkedList<>();
         }
     }
 
-    void addEdge(int source, int destination) {
+    public void addEdge(int source, int destination) {
         adjList[source].add(destination);
         adjList[destination].add(source);
     }
 
-    void display() {
-        for(int i = 0; i < vertices; i++) {
+    public void display() {
+        for (int i = 0; i < vertices; i++) {
             int listSize = adjList[i].size();
-            if(listSize > 0) {
+            if (listSize > 0) {
                 System.out.print(String.format("Vertex %s is connected to: ", i));
-                for(int j = 0; j < listSize; j++) {
+                for (int j = 0; j < listSize; j++) {
                     System.out.print(adjList[i].get(j) + " ");
                 }
                 System.out.println();
